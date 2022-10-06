39955b928ec8677bda1374303f8f3fbc524ec7b4
anandraguraman <113701095+anandraguraman@users.noreply.github.com>
2022-10-06 09:45:50 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,9 @@
+game.splash("Let's calculate the area of a trapezoid")
+let base_A = game.askForNumber("Enter a base length of the trapezoid (cm)")
+let Base_B = game.askForNumber("Enter the other base length of the trapezoid (cm)")
+let height = game.askForNumber("\"Enter height of trapezoid (cm)\"")
+let AB = base_A + Base_B
+let AB2 = AB + 2
+let Area_of_trapezoid = AB2 * height
+game.splash("I think")
+game.splash("final answer" + Area_of_trapezoid, "cm")
